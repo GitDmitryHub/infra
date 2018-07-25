88db5079e5cfd9f3fd57d52ca43c833d1d087f82
GitDmitryHub <example@example.com>
2018-07-25 16:39:40 +0300

    start ansible

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -7,6 +7,7 @@ module "app" {
   source          = "../modules/app"
   public_key_path = "${var.public_key_path}"
   app_disk_image  = "${var.app_disk_image}"
+  db_ip           = "${module.db.db_internal_ip}"
 }
 
 module "db" {
